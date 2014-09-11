3af45e69180194d272359f59eafef2b695b4116f
Fagner Gomes <fagnerngomes@hotmail.com>
2014-09-11 17:39:18 -0300

    fist commit

---
+++ src/br/com/bluetoothsample/LM_Fragment.java
@@ -0,0 +1,21 @@
+package br.com.bluetoothsample;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class LM_Fragment extends Fragment{
+   @Override
+   public View onCreateView(LayoutInflater inflater,
+      ViewGroup container, Bundle savedInstanceState) {
+      /**
+       * Inflate the layout for this fragment
+       */
+      return inflater.inflate(
+      R.layout.startstop_ls_fragment, container, false);
+   }
+}
